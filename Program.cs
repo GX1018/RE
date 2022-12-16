cb7ba363be5fb1f31e69104b6b3e5818c15686d8
GX1018 <wjdghlxo@gmail.com>
2022-12-16 10:56:21 +0900

    v 0.0.1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fristprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
